67dbaf80c19abce82248ec385e56ea8ade44b658
+ <kibetg984@gmail.com>
2024-10-05 12:53:57 +0300

    Made changes on The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    // Target the paragraph element with id="text"
+    const textElement = document.getElementById("text");
+  
+    // Change the text content of the paragraph
+    textElement.textContent = "This is really cool!";
+  });
+  
